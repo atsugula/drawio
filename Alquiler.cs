dc697b54ae91671af6409b05c3493c6f5467d71f
atsugula <111021816+atsugula@users.noreply.github.com>
2023-12-01 10:23:50 -0500

    Agregar archivos de proyecto.

---
+++ Alquiler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Vehiculo
+{
+    // Atributos
+    public string Marca { get; set; }
+    public string Placa { get; set; }
+    public int Año { get; set; }
+    public string Modelo { get; set; }
+    public bool Disponible { get; set; }
+    public bool Seleccionado { get; set; }
+
+    // Constructor
+    public Vehiculo(string marca, string placa, int año, string modelo, bool disponible, bool seleccionado)
+    {
+        Marca = marca;
+        Placa = placa;
+        Año = año;
+        Modelo = modelo;
+        Disponible = disponible;
+        Seleccionado = seleccionado;
+    }
+
+}
